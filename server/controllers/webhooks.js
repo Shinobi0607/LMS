68317f695fa54e12d7dbca01784c4054fffd87e6
Gaurav Kumar Singh <gaurav.singh@roadbounce.com>
2026-01-11 13:20:16 +0530

    debug

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import User from "../models/user.js";
 import Stripe from "stripe";
 import { Purchase } from "../models/Purchase.js";
-import Course from "../modelscourse.js";
+import Course from "../models/course.js";
 
 // API Controller Function to Manage Clerk User with database
 export const clerkWebhooks = async (req, res) => {
